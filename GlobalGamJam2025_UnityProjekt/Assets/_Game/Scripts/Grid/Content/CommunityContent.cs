f8d0607ae5f827e4a65b25138b2861a7d57da70a
Andreas Hanuja <hanuja2@gmail.com>
2025-01-25 16:55:41 +0100

    fix presenter ref

--- GlobalGamJam2025_UnityProjekt/Assets/_Game/Scripts/Grid/Content/CommunityContent.cs
+++ GlobalGamJam2025_UnityProjekt/Assets/_Game/Scripts/Grid/Content/CommunityContent.cs
@@ -1,10 +1,12 @@
+using Game.Community;
+
 namespace Game.Grid.Content
 {
     public class CommunityContent : AGridContent
     {
         public enum Community { Facepalm, Tiktak, Y, Whatsup}
 
-        public CommunityContent contentReference;
+        public CommunityPresenter communityPresenter;
 
     }
 }
