2dabf10793730a50c5ad2601fb2d547be213b257
Armin <Chain132@web.de>
2025-01-25 12:46:01 +0100

    Added OneFieldMovementProvider

---
+++ GlobalGamJam2025_UnityProjekt/Assets/_Game/Scripts/Unit/AbilityProviders/AOEAbilityProvider.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.Unit
+{
+    public class AOEAbilityProvider : MonoBehaviour, IAbilityProvider
+    {
+        public List<AAbility> GetAbilityPossibilities(Vector2Int position)
+        {
+            List<AAbility> abilityList = new List<AAbility>();
+
+
+            return abilityList;
+        }
+    }
+}
