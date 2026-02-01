1ebd5cea4a5d49ed02b107746b7535beb1d4df16
NutPea <p.nuss96@outlook.de>
2026-02-01 15:06:47 +0100

    SoundManager

--- MasterMaskMaker/Assets/Scripts/SSoundManager.cs
+++ MasterMaskMaker/Assets/Scripts/SSoundManager.cs
@@ -11,64 +11,62 @@ public class SSoundManager : MonoBehaviour
     }
 
 
-    [EventRef]
-    public string GenericButton;
-
-    [EventRef]
-    public string HoverButton;
-
-    [EventRef] 
-    public string UI_Trash;
-
-    [EventRef]
-    public string UI_SellMask;
-
-    [EventRef]
-    public string UI_Scissors;
+   
+    public FMODUnity.EventReference GenericButton;
 
 
-    [EventRef]
-    public string UI_Endscore;
-
-    [EventRef]
-    public string UI_DropMaskshape;
-
-    [EventRef]
-    public string UI_DropItem;
-
-    [EventRef]
-    public string UI_DropColor;
-
-    [EventRef]
-    public string UI_ClickCategory;
-
-    [EventRef]
-    public string UI_10SecTimer;
-    [EventRef]
-    public string NPC_GhostSpawning;
-    [EventRef]
-    public string NPC_GhostLong_Sad;
-    [EventRef]
-    public string NPC_GhostLong_Hello;
-    [EventRef]
-    public string NPC_GhostLong_Happy;
-
-    [EventRef]
-    public string NPC_GhostLittle_Sad;
-    [EventRef]
-    public string NPC_GhostLittle_Hello;
-    [EventRef]
-    public string NPC_GhostLittle_Happy;
-
-    [EventRef]
-    public string NPC_GhostBig_Hello;
-    [EventRef]
-    public string NPC_GhostBig_Happy;
-    [EventRef]
-    public string NPC_GhostBig_Sad;
+    public FMODUnity.EventReference HoverButton;
 
 
-    public void PlaySound(string path)
+    public FMODUnity.EventReference UI_Trash;
+
+
+    public FMODUnity.EventReference UI_SellMask;
+
+    public FMODUnity.EventReference UI_Scissors;
+
+
+
+    public FMODUnity.EventReference UI_Endscore;
+
+    public FMODUnity.EventReference UI_DropMaskshape;
+
+
+    public FMODUnity.EventReference UI_DropItem;
+
+
+    public FMODUnity.EventReference UI_DropColor;
+
+
+    public FMODUnity.EventReference UI_ClickCategory;
+
+
+    public FMODUnity.EventReference UI_10SecTimer;
+
+    public FMODUnity.EventReference NPC_GhostSpawning;
+
+    public FMODUnity.EventReference NPC_GhostLong_Sad;
+
+    public FMODUnity.EventReference NPC_GhostLong_Hello;
+
+    public FMODUnity.EventReference NPC_GhostLong_Happy;
+
+
+    public FMODUnity.EventReference NPC_GhostLittle_Sad;
+
+    public FMODUnity.EventReference NPC_GhostLittle_Hello;
+
+    public FMODUnity.EventReference NPC_GhostLittle_Happy;
+
+
+    public FMODUnity.EventReference NPC_GhostBig_Hello;
+
+    public FMODUnity.EventReference NPC_GhostBig_Happy;
+
+    public FMODUnity.EventReference NPC_GhostBig_Sad;
+
+
+    public void PlaySound(FMODUnity.EventReference path)
     {
         RuntimeManager.PlayOneShot(path);
     }
